9b4868e62be12f63a66bc0cdc63871ff8339ce9f
yeongwei <ywlai@origineit.com>
2015-11-19 00:27:51 +0800

    implement fqn member variables

--- src/com/psl/cognos/model/crosslaunch/component/CounterReference.java
+++ src/com/psl/cognos/model/crosslaunch/component/CounterReference.java
@@ -1,5 +1,38 @@
 package com.psl.cognos.model.crosslaunch.component;
 
-public class CounterReference {
+import com.psl.cognos.model.crosslaunch.parser.Parser;
 
+public class CounterReference {
+  
+  public final String fqnPath; // From business layer
+  public final String fqnName; // From business layer
+  
+  public String presentationFqnPath;
+  
+  public CounterReference(String fqn) {
+    this(Parser.getFqnPath(fqn), Parser.getFqnName(fqn));
+  }
+  
+  public CounterReference(String fqnPath, String fqnName) {
+    this.fqnPath = fqnPath;
+    this.fqnName = fqnName;
+  }
+  
+  public boolean setPresentationFqnPath(String presentationFqnPath) {
+    this.presentationFqnPath = presentationFqnPath;
+    return true;
+  }
+  
+  @Override
+  public String toString() {
+    StringBuffer s = new StringBuffer();
+    
+    if (this.presentationFqnPath == null) {
+      s.append(this.fqnPath).append(".");
+    } else {
+      s.append(this.presentationFqnPath).append(".");
+    }
+    s.append(this.fqnName);
+    return s.toString();
+  }
 }
