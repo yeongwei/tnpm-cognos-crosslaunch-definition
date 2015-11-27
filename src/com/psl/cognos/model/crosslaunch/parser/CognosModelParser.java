273afcdde419f93be9b49fcfff2c4590e5a78642
yeongwei <ywlai@origineit.com>
2015-11-27 13:23:46 +0800

    Avoid warning of unused member

--- src/com/psl/cognos/model/crosslaunch/parser/CognosModelParser.java
+++ src/com/psl/cognos/model/crosslaunch/parser/CognosModelParser.java
@@ -1,23 +1,20 @@
 package com.psl.cognos.model.crosslaunch.parser;
 
-import java.util.ArrayList;
 import java.util.logging.Logger;
-
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import com.psl.cognos.model.crosslaunch.component.Pair;
 import com.psl.cognos.model.crosslaunch.meta.ModelNodeName;
 
 public class CognosModelParser {
   private static Logger LOGGER = Logger.getLogger(CognosModelParser.class
       .getName());
+  
+  public CognosModelParser() {
+    LOGGER.info("This is constructor.");
+  }
 
   /**
    * Get values of name tag within a node E.g. <node><name>getThis</name></node>
