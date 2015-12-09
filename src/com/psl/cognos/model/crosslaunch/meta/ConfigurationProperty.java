0e39abc9be397d53aac5b7d0ecf4e244b7b24715
yeongwei <ywlai@origineit.com>
2015-12-09 18:22:27 +0800

    Added PROPERTY for Counter Friendly Name file

--- src/com/psl/cognos/model/crosslaunch/meta/ConfigurationProperty.java
+++ src/com/psl/cognos/model/crosslaunch/meta/ConfigurationProperty.java
@@ -2,11 +2,12 @@ package com.psl.cognos.model.crosslaunch.meta;
 
 /**
  * Facilitate information on -D* JVM arguments
+ * 
  * @author laiyw
- *
+ * 
  */
 public enum ConfigurationProperty {
-  COGNOS_MODEL_FILE, ALARM_MODEL_FILE;
+  COGNOS_MODEL_FILE, ALARM_MODEL_FILE, COUNTER_FRIENDLY_NAME_FILE;
 
   public String getName() {
     switch (this) {
@@ -14,6 +15,8 @@ public enum ConfigurationProperty {
       return "cognosModelFile";
     case ALARM_MODEL_FILE:
       return "alarmModelFile";
+    case COUNTER_FRIENDLY_NAME_FILE:
+      return "counterFriendlyNameFile";
     default:
       return null;
     }
